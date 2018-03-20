f40fd9f6f73f4ee2ead4da48ad92d2346dc0e903
Nex <nexzhu@gmail.com>
2018-03-20 18:47:31 +0800

    Add Ace editor

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import convert from './convert'
+import nex from './nex.json'
 
 window.onload = () => {
+
+  const editor = ace.edit("editor")
+  const JsonMode = ace.require("ace/mode/json").Mode
+  editor.session.setMode(new JsonMode())
+  editor.setValue(JSON.stringify(nex, null, '  '))
+
   const km = window.km = new kityminder.Minder()
   km.renderTo('#minder-view')
   generate()
@@ -8,18 +15,18 @@ window.onload = () => {
   km.execCommand('hand')
   document.querySelector('#generate-btn').onclick = generate
   document.querySelector('#save-btn').onclick = save
-}
 
-function generate() {
-  km.importData('json', convert(document.querySelector('#skillset-input').value))
-}
+  function generate() {
+    km.importData('json', convert(editor.getValue()))
+  }
 
-function save() {
-  km.exportData('png').then((url) => {
-    let download = document.createElement('a')
-    download.href = url
-    download.download = 'skill-set.png'
-    download.click()
-    document.querySelector('canvas').remove()
-  })
+  function save() {
+    km.exportData('png').then((url) => {
+      const download = document.createElement('a')
+      download.href = url
+      download.download = 'skill-set.png'
+      download.click()
+      document.querySelector('canvas').remove()
+    })
+  }
 }
